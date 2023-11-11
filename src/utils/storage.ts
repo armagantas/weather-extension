22e3f79024fdf5f5b247eeeb1f59278be12de480
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2023-11-12 02:15:13 +0300

    Storage part done.

---
+++ src/utils/storage.ts
@@ -0,0 +1,30 @@
+export interface LocalStorage {
+  cities?: string[];
+}
+
+export type LocalStorageKeys = keyof LocalStorage;
+
+export function setStoredCities(cities: string[]): Promise<void> {
+  const vals: LocalStorage = {
+    cities,
+  };
+  return new Promise((resolve) => {
+    chrome.storage.local.set(
+      {
+        cities: vals.cities,
+      },
+      () => {
+        resolve();
+      }
+    );
+  });
+}
+
+export function getStoredCities(): Promise<string[]> {
+  const keys: LocalStorageKeys[] = ["cities"];
+  return new Promise((resolve) => {
+    chrome.storage.local.get(keys, (res: LocalStorage) => {
+      resolve(res.cities ?? []);
+    });
+  });
+}
